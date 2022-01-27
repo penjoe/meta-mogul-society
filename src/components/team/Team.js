885b03e91ea815c3e39d8e3fb1d931a83314a129
Joe Pennock <pennockjoe@gmail.com>
2022-01-27 02:00:39 -0800

    setup basic roadmap component

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -9,7 +9,7 @@ import TeamCard from './TeamCard.js';
 
 const Team = () => {
   return(
-    <div className='team-wrap' style={{ backgroundColor: '#ffffff' }}>
+    <div className='team-wrap'>
       <h1 className='team-header'>Meet the team</h1>
       <div className='team'>
         <TeamCard order={'order1'} img={images.burlmanTeamAvatar} name={'Burlman'} text={'this is burlman'} />
--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
-// stylesheets
-import './styles/App.css';
-import './styles/Header.css';
-import './styles/Team.css';
-import './styles/Footer.css';
-
 // root App component
 import App from './components/App.js';
 
